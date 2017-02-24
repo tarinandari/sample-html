20be0319339f4b1f710d655002c17882debde194
tarinandari <tari_s@outlook.com>
2017-02-24 17:42:58 +0800

    Change the app name and group name to TR-Stack

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,8 +4,8 @@ import groovy.json.JsonSlurperClassic
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
-	def applicationName = 'agusadi-app' //change me
-	def deploymentGroupName = 'agusadi-app' // change me
+	def applicationName = 'TR-Stack' //change me
+	def deploymentGroupName = 'TR-Stack' // change me
 	def s3BucketName = 'deployment-cdc'
 	
 	//Cleanup workspace
